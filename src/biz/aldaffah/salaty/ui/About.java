d696270b76f677f2cd7506e576175f98a20634e3
boudeffa <boudeffa@gmail.com>
2015-04-02 17:44:08 +0200

    Feedback is essential, thank you all

--- src/biz/aldaffah/salaty/ui/About.java
+++ src/biz/aldaffah/salaty/ui/About.java
@@ -1,46 +1,19 @@
 package biz.aldaffah.salaty.ui;
 
-import biz.aldaffah.salaty.R;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
+
+import biz.aldaffah.salaty.R;
 
 public class About extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.about);// TODO create a new layout
-		Button feedback = (Button) findViewById(R.id.feadbackButton);
-		feedback.setOnClickListener(new OnClickListener() {
-
-			public void onClick(View v) {
-				Intent i = new Intent(Intent.ACTION_SEND);
-				i.setType("message/rfc822");
-				i.putExtra(Intent.EXTRA_EMAIL, new String[] { About.this
-						.getString(R.string.feedbackEmail) });
-				i.putExtra(Intent.EXTRA_SUBJECT,
-						About.this.getString(R.string.feedbackSubject));
-				i.putExtra(Intent.EXTRA_TEXT,
-						About.this.getString(R.string.feedbackBody));
-				try {
-					startActivity(Intent.createChooser(i, About.this
-							.getString(R.string.feedbackChooserString)));
-				} catch (android.content.ActivityNotFoundException ex) {
-					Toast.makeText(About.this,
-							About.this.getString(R.string.feedbackFailed),
-							Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
 		
 		/*
 		 * Lot of buttons is not good
-		
+
 		Button contactus = (Button) findViewById(R.id.contactUsButton);
 		contactus.setOnClickListener(new OnClickListener() {
 
@@ -63,7 +36,7 @@ public class About extends Activity {
 				}
 			}
 		});
-		 */
+		*/
 
 	}
 
